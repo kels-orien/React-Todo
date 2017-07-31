0fb91d90adc30b8f5f71434e53501505d0178abe
kels-orien <kelvinorien@gmail.com>
2017-07-31 16:40:12 +0100

    added firebase configuration

---
+++ app/firebase/index.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+try {
+  var config = {
+      apiKey: "AIzaSyDRfIpzjR8Qqr4ICdOQB7MdL4lpIQb0qCc",
+      authDomain: "todo-app-b0468.firebaseapp.com",
+      databaseURL: "https://todo-app-b0468.firebaseio.com",
+      projectId: "todo-app-b0468",
+      storageBucket: "gs://todo-app-b0468.appspot.com",
+      messagingSenderId: "773410644700"
+    };
+
+  firebase.initializeApp(config);
+} catch (e) {
+
+}
+
+export var firebaseRef = firebase.database().ref();
+export default firebase;
