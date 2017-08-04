1b774a879543aec9b4b3a2eac3a93eb8b52d82cd
kels-orien <kelvinorien@gmail.com>
2017-08-04 20:57:26 +0100

    edited config to reduce package size

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
@@ -14,6 +16,11 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
     })
   ],
   output: {
@@ -25,14 +32,14 @@ module.exports = {
     modulesDirectories: [
       'node_modules',
       './app/components',
-        './app/api'
+      './app/api'
     ],
     alias: {
       app: 'app',
       applicationStyles: 'app/styles/app.scss',
-       actions: 'app/actions/actions.jsx',
-       reducers: 'app/reducers/reducers.jsx',
-       configureStore:'app/store/configureStore.jsx'
+      actions: 'app/actions/actions.jsx',
+      reducers: 'app/reducers/reducers.jsx',
+      configureStore: 'app/store/configureStore.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
@@ -53,5 +60,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
